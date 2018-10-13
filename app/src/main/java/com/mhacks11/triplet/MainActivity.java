4faa44a664b1a619fe7c0f3ab97cd4362e91cdb0
Chase Marshall <chaseam@umich.edu>
2018-10-13 17:54:59 -0400

    Added the FireBase authentication service as well as part of the FireBase database services

--- app/src/main/java/com/mhacks11/triplet/MainActivity.java
+++ app/src/main/java/com/mhacks11/triplet/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
