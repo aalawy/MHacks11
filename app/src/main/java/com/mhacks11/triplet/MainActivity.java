77702f48ee0ae5ff5a61f7bcb1d90f6720f4b436
Moustafa <mamutahr@umich.edu>
2018-10-13 16:38:29 -0400

    Initial Commit, basic tic tac toe

---
+++ app/src/main/java/com/mhacks11/triplet/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mhacks11.triplet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button beginGame = findViewById(R.id.beginButton);
+        beginGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
