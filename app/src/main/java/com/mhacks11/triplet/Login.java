259ce259f05df211c93676572532c55e9486586b
Moustafa <mamutahr@umich.edu>
2018-10-14 05:47:07 -0400

    login, signin, sign out, all work

--- app/src/main/java/com/mhacks11/triplet/Login.java
+++ app/src/main/java/com/mhacks11/triplet/Login.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Patterns;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,20 +17,92 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.mhacks11.triplet.Model.User;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
-    Button btnSignUp, btnLogin;
-    private FirebaseAuth mAuth;
+
+    private EditText editEmail, editPassword;
+    private Button btnSignin, btnLogin;
     FirebaseAuth.AuthStateListener mAuthListener;
-    EditText editEmail, editUsername, editPassword;
+    private FirebaseAuth mAuth;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public  void  onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    Intent intent = new Intent(Login
+                            .this, Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+
+        };
+
+        editEmail = (EditText) findViewById(R.id.email);
+        editPassword = (EditText) findViewById(R.id.password);
+
+        btnSignin = (Button) findViewById(R.id.btnSignUp);
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
+
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                if (email.isEmpty()) {
+                    editEmail.setError("Email is required");
+                    editEmail.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    editPassword.setError("Password is required");
+                    editPassword.requestFocus();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            finish();
+                            startActivity(new Intent(Login.this, Home.class));
+                        } else {
+
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    }
+                });
+            }
+        });
+
+    }
 
     @Override
     public void onStart() {
@@ -46,87 +119,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener(){
-            @Override
-            public  void  onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if(user!=null){
-                    Intent intent = new Intent(MainActivity
-                            .this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-
-
-        };
-
-        editUsername = (EditText) findViewById(R.id.username);
-        editEmail = (EditText) findViewById(R.id.email);
-        editPassword = (EditText) findViewById(R.id.password);
-
-        btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Login.class));
-            }
-        });
-
-
-        btnSignUp = (Button) findViewById(R.id.btnSignUp);
-
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                final User user = new User(editUsername.getText().toString(),
-                        editEmail.getText().toString(),
-                        editPassword.getText().toString());
-
-                String email = user.getEmail();
-                String password = user.getPassword();
-
-                if (email.isEmpty()) {
-                    editEmail.setError("Email is required");
-                    editEmail.requestFocus();
-                    return;
-                }
-
-                if (password.isEmpty()) {
-                    editPassword.setError("Password is required");
-                    editPassword.requestFocus();
-                    return;
-                }
-
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            finish();
-                            startActivity(new Intent(MainActivity.this, Home.class));
-                        } else {
-
-                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
-                                Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
-
-                            } else {
-                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                            }
-
-                        }
-                    }
-                });
-            }
-        });
-
-    }
 }
